c7f2b7d6ab5faa6461ee70aa90280a8cd7b3c181
smaster28 <santiago.arias.2805@gmail.com>
2024-11-22 21:16:56 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Dimensiones del tablero
+BOARD_SIZE = 10
+
+# Configuración de los barcos
+SHIPS = {
+    "Barco 1": 1,  # 4 barcos de 1 posición
+    "Barco 2": 2,  # 3 barcos de 2 posiciones
+    "Barco 3": 3,  # 2 barcos de 3 posiciones
+    "Barco 4": 4   # 1 barco de 4 posiciones
+}
+
+# Opciones de valores para el tablero
+WATER = "0"  # Agua
+SHIP = "1"   # Barco
+HIT = "X"    # Impacto
+MISS = "M"   # Agua disparada
